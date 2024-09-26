8558b28aff0957ef824ad78fa699eaf88079b726
Felipe De Andrade <felipe.deam98@gmail.com>
2024-09-26 01:11:33 -0300

    styless: Split File

--- src/components/ErrorMenssage/index.tsx
+++ src/components/ErrorMenssage/index.tsx
@@ -1,14 +1,4 @@
-import styled from "styled-components";
-
-const ErrorMenssageStyled = styled.span`
-  position: absolute;
-  left: 0;
-  top: calc(100% + .5rem);
-
-  color: var(--error);
-  font-size: 1.4rem;
-  font-weight: bold;
-`
+import { ErrorMenssageStyled } from "./styles";
 
 const ErrorMenssage = ({ text }: { text: string }) => {
   return (
---
+++ src/components/ErrorMenssage/styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ErrorMenssageStyled = styled.span`
+  position: absolute;
+  left: 0;
+  top: calc(100% + .5rem);
+  color: var(--error);
+  font-size: 1.4rem;
+  font-weight: bold;
+`
